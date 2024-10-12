0db7fb7a91e90f97a7ffe725f7e488f40f115544
Arif Khan <khanarif2738@gmail.com>
2024-10-12 12:47:07 +0530

    UI part are completed

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import todo_icon from '../assets/todo_icon.png';
+import TodoItem from './TodoItem';
 
 const Todo = () => {
   return (
@@ -11,14 +12,20 @@ const Todo = () => {
             <h1 className='text-3xl font-semibold'>To-Do List</h1>
         </div>
 
-     {/* --------- input box --------- */}
+    {/* --------- input box --------- */}
 
         <div className='flex items-center my-7 bg-gray-200 rounded-full'>
             <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task'/>
             <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
         </div>
-
+    
+    {/* --------- todo list --------- */}
+        <div>
+            <TodoItem text="Learn Coding"/>
+            <TodoItem text="Learn Coding from Arif khan"/>
+        </div>
         
+
     </div>
   )
 }
