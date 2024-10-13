e1ec85f7b1da0e7925768a2ea17397bf7b8893e8
Arif Khan <khanarif2738@gmail.com>
2024-10-13 10:36:10 +0530

    using useRef hook we collect data from input field

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import todo_icon from '../assets/todo_icon.png';
 import TodoItem from './TodoItem';
 
 const Todo = () => {
+
+    const inputRef = useRef();
+
+    const add = () => {
+        const inputText = inputRef.current.value.trim(); 
+        console.log(inputText);
+    }
+
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
 
@@ -15,8 +23,8 @@ const Todo = () => {
     {/* --------- input box --------- */}
 
         <div className='flex items-center my-7 bg-gray-200 rounded-full'>
-            <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task'/>
-            <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
+            <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task'/>
+            <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
         </div>
     
     {/* --------- todo list --------- */}
